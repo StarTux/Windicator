eca1f23afbddcecacf770de06fc62179d60e29b6
StarTux <ths.loy@gmail.com>
2025-09-06 17:48:47 +0200

    Refactor commands

--- src/main/java/com/cavetale/windicator/WindicatorCommand.java
+++ src/main/java/com/cavetale/windicator/WindicatorCommand.java
@@ -1,86 +1,56 @@
 package com.cavetale.windicator;
 
+import com.cavetale.core.command.AbstractCommand;
+import com.cavetale.core.command.CommandArgCompleter;
+import com.cavetale.core.command.CommandWarn;
 import com.cavetale.core.struct.Vec3i;
 import com.cavetale.core.util.Json;
 import com.cavetale.fam.trophy.Highscore;
 import com.cavetale.mytems.item.trophy.TrophyCategory;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.Component;
 import org.bukkit.block.Block;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 import static net.kyori.adventure.text.format.TextDecoration.*;
 
-@RequiredArgsConstructor
-public final class WindicatorCommand implements TabExecutor {
-    final WindicatorPlugin plugin;
-
-    static final class Wrong extends Exception {
-        Wrong(final String msg) {
-            super(msg);
-        }
+public final class WindicatorCommand extends AbstractCommand<WindicatorPlugin> {
+    public WindicatorCommand(final WindicatorPlugin plugin) {
+        super(plugin, "windicator");
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command,
-                             String label, String[] args) {
-        if (args.length == 0) return false;
-        try {
-            return onCommand(sender, args[0], Arrays.copyOfRange(args, 1, args.length));
-        } catch (Wrong w) {
-            sender.sendMessage(text(w.getMessage(), RED));
-            return true;
-        }
+    public void onEnable() {
+        rootNode.addChild("info").denyTabCompletion()
+            .senderCaller(this::info);
+        rootNode.addChild("start").denyTabCompletion()
+            .senderCaller(this::start);
+        rootNode.addChild("stop").denyTabCompletion()
+            .senderCaller(this::stop);
+        rootNode.addChild("victory").denyTabCompletion()
+            .senderCaller(this::victory);
+        rootNode.addChild("addcore").arguments("<type>")
+            .completers(CommandArgCompleter.enumLowerList(CoreType.class))
+            .playerCaller(this::addCore);
+        rootNode.addChild("removecore").arguments("<type>")
+            .completers(CommandArgCompleter.enumLowerList(CoreType.class))
+            .playerCaller(this::removeCore);
+        rootNode.addChild("clearCores").denyTabCompletion()
+            .senderCaller(this::clearCores);
+        rootNode.addChild("clearscores").denyTabCompletion()
+            .senderCaller(this::clearScores);
+        rootNode.addChild("rewardscores").denyTabCompletion()
+            .senderCaller(this::rewardScores);
     }
 
-    @Override
-    public List<String> onTabComplete(CommandSender sender, Command command,
-                                      String label, String[] args) {
-        if (args.length == 0) return null;
-        String cmd = args[0];
-        String arg = args[args.length - 1];
-        if (args.length == 1) {
-            return complete(cmd, Stream.of("info",
-                                           "start", "stop",
-                                           "victory",
-                                           "addcore", "removecore", "clearcores",
-                                           "clearscores", "rewardscores"));
-        }
-        if (args.length == 2) {
-            switch (args[0]) {
-            case "addcore": case "removecore": {
-                final String lower = args[1].toLowerCase();
-                List<String> result = new ArrayList<>();
-                for (CoreType coreType : CoreType.values()) {
-                    final String name = coreType.name().toLowerCase();
-                    if (name.contains(lower)) result.add(name);
-                }
-                return result;
-            }
-            default: break;
-            }
-        }
-        return List.of();
+    private void info(CommandSender sender) {
+        sender.sendMessage("isValid=" + plugin.getWindicator().isValid());
+        sender.sendMessage("state=" + Json.serialize(plugin.getWindicator().getState()));
     }
 
-    public boolean onCommand(CommandSender sender, String cmd, String[] args) throws Wrong {
-        switch (cmd) {
-        case "info": {
-            if (args.length != 0) return false;
-            sender.sendMessage("isValid=" + plugin.getWindicator().isValid());
-            sender.sendMessage("state=" + Json.serialize(plugin.getWindicator().getState()));
-            return true;
-        }
-        case "start": {
+    private void start(CommandSender sender) {
             plugin.getWindicator().setEnabled(true);
             plugin.getWindicator().save();
             if (plugin.getWindicator().isValid()) {
@@ -88,101 +58,69 @@ public final class WindicatorCommand implements TabExecutor {
             } else {
                 sender.sendMessage("Game started but still not valid.");
             }
-            return true;
-        }
-        case "stop": {
-            plugin.getWindicator().setEnabled(false);
-            plugin.getWindicator().save();
-            sender.sendMessage("Game stopped");
-            return true;
-        }
-        case "victory": {
-            plugin.getWindicator().setVictory(!plugin.getWindicator().isVictory());
-            plugin.getWindicator().save();
-            sender.sendMessage("Victory set to: " + plugin.getWindicator().isVictory());
-            return true;
-        }
-        case "addcore": {
-            if (args.length != 1) return false;
-            Player player = playerOf(sender);
-            String name = args[0];
-            final CoreType coreType;
-            try {
-                coreType = CoreType.valueOf(name.toUpperCase());
-            } catch (IllegalArgumentException iae) {
-                throw new Wrong("Unknown core type: " + name);
-            }
-            Block block = player.getTargetBlockExact(5);
-            if (block == null) throw new Wrong("Not looking at block");
-            if (!plugin.getWindicator().addCore(block, coreType)) {
-                throw new Wrong("Core block already contained.");
-            }
-            plugin.getWindicator().save();
-            sender.sendMessage("Core block added: " + coreType + ": " + Vec3i.of(block));
-            return true;
-        }
-        case "removecore": {
-            if (args.length != 1) return false;
-            Player player = playerOf(sender);
-            String name = args[0];
-            final CoreType coreType;
-            try {
-                coreType = CoreType.valueOf(name.toUpperCase());
-            } catch (IllegalArgumentException iae) {
-                throw new Wrong("Unknown core type: " + name);
-            }
-            Block block = player.getTargetBlockExact(5);
-            if (block == null) throw new Wrong("Not looking at block");
-            boolean res = plugin.getWindicator().removeCore(block, coreType, true);
-            if (res) {
-                plugin.getWindicator().save();
-                sender.sendMessage("Core block removed: " + coreType + ": " + Vec3i.of(block));
-            } else {
-                throw new Wrong("Not a core block: " + Vec3i.of(block));
-            }
-            return true;
-        }
-        case "clearcores": {
-            if (args.length != 0) return false;
-            plugin.getWindicator().clearCores();
-            plugin.getWindicator().save();
-            sender.sendMessage("Cores cleared.");
-            return true;
-        }
-        case "clearscores": {
-            plugin.getWindicator().getState().getScores().clear();
-            plugin.getWindicator().computeHighscore();
-            sender.sendMessage(text("Scores cleared", YELLOW));
-            return true;
-        }
-        case "rewardscores": {
-            final int count = Highscore.reward(plugin.getWindicator().getState().getScores(),
-                                               "windicator",
-                                               TrophyCategory.SWORD,
-                                               text("Windicator", GOLD, BOLD),
-                                               hi -> "You collected " + hi.score + " point" + (hi.score == 1 ? "" : "s"));
-            List<Highscore> highscore = Highscore.of(plugin.getWindicator().getState().getScores());
-            List<Component> highscoreLines = Highscore.sidebar(highscore, TrophyCategory.SWORD);
-            for (Component line : highscoreLines) {
-                sender.sendMessage(line);
-            }
-            sender.sendMessage(text(count + " players rewarded", YELLOW));
-            return true;
-        }
-        default:
-            return false;
-        }
     }
 
-    List<String> complete(String arg, Stream<String> args) {
-        return args.filter(a -> a.startsWith(arg))
-            .collect(Collectors.toList());
+    private void stop(CommandSender sender) {
+        plugin.getWindicator().setEnabled(false);
+        plugin.getWindicator().save();
+        sender.sendMessage("Game stopped");
     }
 
-    Player playerOf(CommandSender sender) throws Wrong {
-        if (!(sender instanceof Player)) {
-            throw new Wrong("Player expected");
+    private void victory(CommandSender sender) {
+        plugin.getWindicator().setVictory(!plugin.getWindicator().isVictory());
+        plugin.getWindicator().save();
+        sender.sendMessage("Victory set to: " + plugin.getWindicator().isVictory());
+    }
+
+    private boolean addCore(Player player, String[] args) {
+        if (args.length != 1) return false;
+        final CoreType coreType = CommandArgCompleter.requireEnum(CoreType.class, args[0]);
+        final Block block = player.getTargetBlockExact(5);
+        if (block == null) throw new CommandWarn("Not looking at block");
+        if (!plugin.getWindicator().addCore(block, coreType)) {
+            throw new CommandWarn("Core block already contained.");
         }
-        return (Player) sender;
+        plugin.getWindicator().save();
+        player.sendMessage("Core block added: " + coreType + ": " + Vec3i.of(block));
+        return true;
+    }
+
+    private boolean removeCore(Player player, String[] args) {
+        if (args.length != 1) return false;
+        final CoreType coreType = CommandArgCompleter.requireEnum(CoreType.class, args[0]);
+        final Block block = player.getTargetBlockExact(5);
+        if (block == null) throw new CommandWarn("Not looking at block");
+        if (!plugin.getWindicator().removeCore(block, coreType, true)) {
+            throw new CommandWarn("Not a core block: " + Vec3i.of(block));
+        }
+        plugin.getWindicator().save();
+        player.sendMessage("Core block removed: " + coreType + ": " + Vec3i.of(block));
+        return true;
+    }
+
+    private void clearCores(CommandSender sender) {
+        plugin.getWindicator().clearCores();
+        plugin.getWindicator().save();
+        sender.sendMessage("Cores cleared.");
+    }
+
+    private void clearScores(CommandSender sender) {
+        plugin.getWindicator().getState().getScores().clear();
+        plugin.getWindicator().computeHighscore();
+        sender.sendMessage(text("Scores cleared", YELLOW));
+    }
+
+    private void rewardScores(CommandSender sender) {
+        final int count = Highscore.reward(plugin.getWindicator().getState().getScores(),
+                                           "windicator",
+                                           TrophyCategory.SWORD,
+                                           text("Windicator", GOLD, BOLD),
+                                           hi -> "You collected " + hi.score + " point" + (hi.score == 1 ? "" : "s"));
+        List<Highscore> highscore = Highscore.of(plugin.getWindicator().getState().getScores());
+        List<Component> highscoreLines = Highscore.sidebar(highscore, TrophyCategory.SWORD);
+        for (Component line : highscoreLines) {
+            sender.sendMessage(line);
+        }
+        sender.sendMessage(text(count + " players rewarded", YELLOW));
     }
 }
